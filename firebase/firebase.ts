d7966dbea577e0ea6a164b7db94210c9881923bc
kemal <kemal182182@gmail.com>
2025-05-02 14:42:42 +0100

    secuirty change

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";  // Import Firebase Storage
 
-// Your Firebase config (replace with your actual config)
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDnfQA7HKv38dUjV3GyQoVkJOU7Kq5WqLc",
-  authDomain: "mcars-546ff.firebaseapp.com",
-  projectId: "mcars-546ff",
-  storageBucket: "mcars-546ff.firebasestorage.app",
-  messagingSenderId: "683879598241",
-  appId: "1:404603535310:web:38b1c4ab8d5ed33d2db325"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
